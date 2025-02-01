2fd23f65099bd1bcb4a4ce054ddf263de56a438b
Mike Riess <mikeriess@me.com>
2025-02-01 15:06:25 +0100

    wierd

--- docker_vm/workspace/finetune_vlm.py
+++ docker_vm/workspace/finetune_vlm.py
@@ -1,5 +1,3 @@
-from unsloth import FastVisionModel
-import torch
 import argparse
 
 def parse_args():
@@ -9,25 +7,9 @@ def parse_args():
     return parser.parse_args()
 
 def main():
+    from unsloth import FastVisionModel
+    import torch
     args = parse_args()
-
-    # 4bit pre quantized models we support for 4x faster downloading + no OOMs.
-    fourbit_models = [
-        "unsloth/Llama-3.2-11B-Vision-Instruct-bnb-4bit", # Llama 3.2 vision support
-        "unsloth/Llama-3.2-11B-Vision-bnb-4bit",
-        "unsloth/Llama-3.2-90B-Vision-Instruct-bnb-4bit", # Can fit in a 80GB card!
-        "unsloth/Llama-3.2-90B-Vision-bnb-4bit",
-
-        "unsloth/Pixtral-12B-2409-bnb-4bit",              # Pixtral fits in 16GB!
-        "unsloth/Pixtral-12B-Base-2409-bnb-4bit",         # Pixtral base model
-
-        "unsloth/Qwen2-VL-2B-Instruct-bnb-4bit",          # Qwen2 VL support
-        "unsloth/Qwen2-VL-7B-Instruct-bnb-4bit",
-        "unsloth/Qwen2-VL-72B-Instruct-bnb-4bit",
-
-        "unsloth/llava-v1.6-mistral-7b-hf-bnb-4bit",      # Any Llava variant works!
-        "unsloth/llava-1.5-7b-hf-bnb-4bit",
-    ] # More models at https://huggingface.co/unsloth
 
     model, tokenizer = FastVisionModel.from_pretrained(
     "unsloth/Llama-3.2-11B-Vision-Instruct",
